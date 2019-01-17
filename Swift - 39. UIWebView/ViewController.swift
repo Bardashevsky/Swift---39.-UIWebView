45af4d315bc663779ff680ba71438d8d2c645b7f
Alex Bardashevsky <mijiekc@icloud.com>
2019-01-17 13:44:23 +0200

    Initial Commit

---
+++ Swift - 39. UIWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift - 39. UIWebView
+//
+//  Created by Oleksandr Bardashevskyi on 1/17/19.
+//  Copyright © 2019 Oleksandr Bardashevskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
