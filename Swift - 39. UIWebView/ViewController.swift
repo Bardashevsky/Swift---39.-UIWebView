3c53103e5b76fa54eb7d28153c14755ed11a63ab
Alex Bardashevsky <mijiekc@icloud.com>
2019-01-17 15:24:06 +0200

    Brouser

--- Swift - 39. UIWebView/ViewController.swift
+++ Swift - 39. UIWebView/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var urlTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        urlTextField.delegate = self
+        webView.navigationDelegate = self
+        
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let stringUrl = "google.com"
+        let url = URL(string: "https://"+stringUrl)!
+        let urlReauest: URLRequest = URLRequest(url: url)
+        
+        webView.load(urlReauest)
+        
+        urlTextField.text = stringUrl
+    }
+    @IBAction func backButtonAction(_ sender: UIButton) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    @IBAction func forwardButtonAction(_ sender: UIButton) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let stringUrl = urlTextField.text!
+        let url = URL(string: "https://"+stringUrl)!
+        let urlReauest: URLRequest = URLRequest(url: url)
+        
+        webView.load(urlReauest)
+        textField.resignFirstResponder()
+        return true
+    }
+}
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+        
+        urlTextField.text = webView.url?.absoluteString
+    }
+}
